f8846bd6400ab4de7a34fd68e6fc92abd0d04385
hiroshi <gutenyetzt@yahoo.ne.jp>
2021-11-03 02:08:11 +0000

    Finish japanization-and-time-format

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,7 @@ module AttendanceApp
   class Application < Rails::Application
     config.load_defaults 5.1
     config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
